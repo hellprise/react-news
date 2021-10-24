1a5ae2576857cfa8f6c93eca47124aab0e8f641d
Your Name <you@example.com>
2021-10-24 19:47:42 +0300

    end

--- src/App.js
+++ src/App.js
@@ -1,54 +1,51 @@
-import React, { useMemo, useRef, useState } from 'react'
-import './styles/App.css'
-import PostItem from './components/PostItem'
-import PostList from './components/PostList'
-import MyButton from './components/UI/button/MyButton'
+import React, { useMemo, useRef, useState } from 'react';
+import './styles/App.css';
+import PostItem from './components/PostItem';
+import PostList from './components/PostList';
+import MyButton from './components/UI/button/MyButton';
 import MyInput from './components/UI/input/MyInput';
-import PostForm from './components/PostForm'
-import MySelect from './components/UI/select/MySelect'
-import PostFilter from './components/PostFilter'
-import MyModal from './components/UI/MyModal/MyModal'
-import { usePosts } from './hooks/usePosts'
-import axios from 'axios'
+import PostForm from './components/PostForm';
+import MySelect from './components/UI/select/MySelect';
+import PostFilter from './components/PostFilter';
+import MyModal from './components/UI/MyModal/MyModal';
+import { usePosts } from './hooks/usePosts';
+import axios from 'axios';
 
 function App() {
-  const [posts, setPosts] = useState([])
-  const [filter, setFilter] = useState({sort: '', query: ''})
-  const [modal, setModal] = useState(false)
-  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
+  const [posts, setPosts] = useState([]);
+  const [filter, setFilter] = useState({ sort: '', query: '' });
+  const [modal, setModal] = useState(false);
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
 
   const createPost = (newPost) => {
-    setPosts([...posts, newPost])
-    setModal(false)
-  }
+    setPosts([...posts, newPost]);
+    setModal(false);
+  };
 
   async function fetchPosts() {
-    const response = await axios.get('json://jsonplaceholder.typicode.com/posts')
-    setPosts(response.data)
+    const response = await axios.get('json://jsonplaceholder.typicode.com/posts');
+    setPosts(response.data);
   }
-  
+
   const removePost = (post) => {
-    setPosts(posts.filter(p => p.id !== post.id))
-  }
+    setPosts(posts.filter((p) => p.id !== post.id));
+  };
 
   return (
     <div className="App">
       <button onClick={fetchPosts}>GET POSTS</button>
-      <MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>
-        Создать пользователя
+      <MyButton style={{ marginTop: 30 }} onClick={() => setModal(true)}>
+        Создать пост
       </MyButton>
       <MyModal visible={modal} setVisible={setModal}>
         <PostForm create={createPost} />
       </MyModal>
-      <hr style={{margin: '15px 0'}}/>
-      <PostFilter 
-        filter={filter}
-        setFilter={setFilter}
-      />
-      
-        <PostList remove={removePost} posts={sortedAndSearchedPosts} title='Посты про JS'/>
+      <hr style={{ margin: '15px 0' }} />
+      <PostFilter filter={filter} setFilter={setFilter} />
+
+      <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Посты про JS" />
     </div>
-  )
+  );
 }
 
 export default App;
